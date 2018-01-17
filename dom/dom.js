a6c8eea7a13676746f86fe5a2160c13d40988595
KedAyAyA <caoxun333@gmail.com>
2018-01-17 15:51:07 +0800

    add offset method

--- dom/dom.js
+++ dom/dom.js
@@ -32,3 +32,48 @@ function loadScriptString (code) {
   }
   document.body.appendChild(script)
 }
+
+// 跨浏览器获取样式表 element: link or style
+function getStyleSheet (element) {
+  return element.sheet || element.styleSheet
+}
+
+// 跨浏览器插入样式表
+function insertRule (sheet, selectorText, cssText, position) {
+  if (sheet.insertRule) {
+    sheet.insertRule(selectorText + '{' + cssText + '}', position)
+  } else if (sheet.addRule) {
+    sheet.addRule(selectorText, cssText, position)
+  }
+}
+
+// 跨浏览器删除规则
+function deleteRule (sheet, index) {
+  if (sheet.deleteRule) {
+    sheet.deleteRule(index)
+  } else if (sheet.removeRule) {
+    sheet.removeRule(index)
+  }
+}
+
+// 获取元素左偏移量
+function getElementLeft (element) {
+  var offsetLeft = element.offsetLeft
+  var current = element.offsetParent
+  while (current !== null) {
+    offsetLeft += current.offsetLeft
+    current = current.offsetParent
+  }
+  return offsetLeft
+}
+
+// 获取元素上偏移量
+function getElementTop (element) {
+  var offsetTop = element.offsetTop
+  var current = element.offsetParent
+  while (current !== null) {
+    offsetTop += current.offsetTop
+    current = current.offsetParent
+  }
+  return offsetTop
+}
